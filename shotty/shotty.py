86f1f67f9977893c85844c89060844ca76389612
unknown <Swordmaster38401@yahoo.com>
2020-08-27 16:56:20 -0400

    Handle error from AWSwhen stopping a pendinjg instances or starting a stopping one

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,5 +1,6 @@
 import boto3
 import click
+import botocore
 
 session = boto3.Session(profile_name='shotty')
 ec2 = session.resource('ec2')
@@ -122,7 +123,11 @@ def stop_instances(project):
 
         for i in instances:
             print("Stopping {0}...".format(i.id))
-            i.stop()
+            try:
+                i.stop()
+            except botocore.exceptions.ClientError as e:
+                print(" Cloud not stop {0}. ".format(i.id) + str(e))
+                continue
 
         return
 
@@ -136,7 +141,11 @@ def start_instances(project):
 
         for i in instances:
             print("Starting {0}...".format(i.id))
-            i.start()
+            try:
+                i.start()
+            except botocore.exceptions.ClientError as e:
+                print(" Cloud not start {0}. ".format(i.id) + str(e))
+                continue
 
         return
 if __name__ == '__main__':
